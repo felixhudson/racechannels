aabfe65436cf69debe9f2fc22134b6875e32c344
Felix Hudson <felix.hudson@adelaide.edu.au>
2017-08-30 17:15:42 +0930

    Trying to add gaps to leader board

--- race.go
+++ race.go
@@ -14,7 +14,7 @@ type Event struct {
 type Car struct {
 	Number       int
 	CurrentSpeed int
-	StartPos int
+	StartPos     int
 }
 
 func main() {
@@ -27,7 +27,7 @@ func main() {
 
 	// start a event reader
 
-	board := make(map[int] int)
+	board := make(map[int]int)
 	go leaderBoard(events, board)
 
 	// use a wait group to only quit when all closed
@@ -35,7 +35,7 @@ func main() {
 	c := Car{33, 5, 1}
 	wg.Add(1)
 	go c.speedcar(events, &wg)
-	c1 := Car{5, 16,2}
+	c1 := Car{5, 6, 2}
 	wg.Add(1)
 	go c1.speedcar(events, &wg)
 
@@ -51,22 +51,38 @@ func eventReader(events <-chan Event) {
 	}
 }
 
+func eventPipe(events <-chan Event) {
+
+	//read our events and push them to another service?
+
+	// rabbit mq?
+	// pubsub?
+	// splunk
+}
+
 func leaderBoard(events <-chan Event, board map[int]int) {
+	gaps := make(map[int]time.Duration)
+	lastTime := time.Now()
+	fmt.Println("starting leaderboard")
 	for v := range events {
-	// filter only sector times
+		// filter only sector times
 		if v.Emitter == "sector" {
-			if _,ok:= board[v.Car] ; !ok {
-				board[v.Car] =0
+			if _, ok := board[v.Car]; !ok {
+				board[v.Car] = 0
 			}
-			board[v.Car] = board[v.Car] +1
+			// append a time gap
+			//fmt.Println(lastTime, time.Now())
+			gaps[v.Car] = time.Since(lastTime)
+			lastTime = time.Now()
+			board[v.Car] = board[v.Car] + 1
 		}
 		fmt.Println("----------")
-		printBoard(board)
+		printBoard(board, gaps)
 		fmt.Println("----------")
 	}
 }
 
-func printBoard(data map[int]int) {
+func printBoard(data map[int]int, gaps map[int]time.Duration) {
 	// we need to sort based on the value not key
 	//find min max in values
 	max := 0
@@ -80,24 +96,28 @@ func printBoard(data map[int]int) {
 		}
 	}
 
-
-	for i := max; i >= min ; i-- {
+	for i := max; i >= min; i-- {
 		for k, v := range data {
-			if v == i{
-				fmt.Println("car", k, i)
+			if v == i {
+				fmt.Println("car", k, "Lap:", i)
+				if i == max {
+					fmt.Println("gap to car", "0.000")
+				} else {
+					fmt.Println("gap to car", gaps[k])
+				}
 			}
 		}
 	}
 }
 
-func (c *Car) speedcar (events chan<- Event, wg *sync.WaitGroup) {
+func (c *Car) speedcar(events chan<- Event, wg *sync.WaitGroup) {
 	// delay amount based on car startpos
 	time.Sleep(time.Duration(c.StartPos) * time.Second)
-	for i := 0; i < 5; i++ {
-			d := Event{c.Number, "sector", "time", string(c.CurrentSpeed)}
-			events <- d
-			sectorTime := 1650 - (c.CurrentSpeed *100)
-			time.Sleep(time.Duration( sectorTime) * time.Millisecond)
+	for i := 0; i < 10; i++ {
+		d := Event{c.Number, "sector", "time", string(c.CurrentSpeed)}
+		events <- d
+		sectorTime := 1000 - (c.CurrentSpeed * 100)
+		time.Sleep(time.Duration(sectorTime) * time.Millisecond)
 	}
 	wg.Done()
 }
